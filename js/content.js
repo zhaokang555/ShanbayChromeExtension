3cc0824301a625ee72d30c6016b634a8fafd0425
Zhao Kang <1003994970@qq.com>
2016-04-17 16:50:31 +0800

    使用ajax获取单词的汉语意思

--- js/content.js
+++ js/content.js
@@ -1,5 +1,5 @@
-document.body.onmouseup = (e) => {
-    let selectedTxt = window.getSelection().toString();
+document.body.onmouseup = function () {
+    var selectedTxt = window.getSelection().toString();
     
     // 判断选中的文本是否是英文
     var re = /[a-zA-Z]+/;
@@ -7,6 +7,33 @@ document.body.onmouseup = (e) => {
         return;
     };
 
-    console.log(selectedTxt);
+    console.log('selectedTxt: \n' + selectedTxt);
+
+    searchWord(selectedTxt);
 };
 
+
+function searchWord(word) {
+
+    var request = new XMLHttpRequest();
+    var url = 'https://api.shanbay.com/bdc/search/?word=' + word;
+    
+    request.onreadystatechange = function () { 
+        if (request.readyState === 4) { 
+            if (request.status === 200) {
+                return success(request.responseText);
+            } 
+        } 
+    }
+
+    // 发送请求:
+    request.open('GET', url, true);
+    request.send();
+
+}
+
+
+function success(json) {
+    var re = JSON.parse(json);
+    console.log('汉语意思：\n' + re.data.cn_definition.defn);
+}
