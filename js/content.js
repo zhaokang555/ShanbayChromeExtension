04ffb61c9f8b1178471c5023f682f090dbe18dd5
Zhao Kang <1003994970@qq.com>
2016-04-17 16:35:06 +0800

    判断选中的文本是否是英文

--- js/content.js
+++ js/content.js
@@ -1,5 +1,12 @@
-
 document.body.onmouseup = (e) => {
     let selectedTxt = window.getSelection().toString();
+    
+    // 判断选中的文本是否是英文
+    var re = /[a-zA-Z]+/;
+    if (!re.test(selectedTxt)) {
+        return;
+    };
+
     console.log(selectedTxt);
 };
+
